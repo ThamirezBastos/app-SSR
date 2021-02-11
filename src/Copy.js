08ec7ba76a7796cd8276d3a9765a0d923070b1b5
unknown <thamirez.bastoss@gmail.com>
2021-02-11 16:43:29 -0300

    exemplo de disable javascript

---
+++ src/Copy.js
@@ -0,0 +1,12 @@
+  
+import React from 'react';
+
+const Copy = (props) => {
+   return (
+       <div>
+           <h1>Thamirez Bastos {props.ano}</h1>
+       </div>
+   )
+}
+
+export default Copy;
